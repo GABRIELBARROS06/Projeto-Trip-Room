63716541d51765ed6819839755426220c3f05890
GabrielBarros <bieldbg56@gmail.com>
2024-03-26 15:24:23 -0300

    Terceira Tela pronta

--- app/src/main/java/br/senai/sp/jandira/trippromm/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/trippromm/MainActivity.kt
@@ -15,33 +15,49 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.BeachAccess
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Landscape
+import androidx.compose.material.icons.filled.LocalFlorist
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Snowboarding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.trippromm.ui.theme.TripprommTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -234,7 +250,12 @@ modifier = Modifier
 @Composable
 fun GreetingPreview() {
     TripprommTheme {
-Greeting()
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Greeting()
+        }
     }
 }
 
@@ -303,7 +324,7 @@ Box(modifier = Modifier
 
         )
     Image(painter = painterResource(id = R.drawable.camera), contentDescription = "",
-        modifier =  Modifier
+        modifier = Modifier
             .height(100.dp)
             .width(30.dp)
             .offset(x = 210.dp, y = 180.dp)
@@ -470,5 +491,244 @@ Box(modifier = Modifier
 fun TelaSignUpPreview() {
     TripprommTheme {
         TelaSignUp()
+    }
+}
+
+@Composable
+fun TelaHome(){
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Color(0xC8FFFFFF)),
+) {
+
+  Box(
+    modifier = Modifier
+        .fillMaxWidth()
+        .height(200.dp)
+
+    ) {
+
+    Image(painter = painterResource(id = R.drawable.imagem), contentScale = ContentScale.Crop,contentDescription = "",
+modifier = Modifier
+    .fillMaxWidth()
+
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        ) {
+            Image(painter = painterResource(id = R.drawable.user), contentDescription = "",
+                modifier = Modifier
+
+                    .size(width = 100.dp, height = 80.dp)
+                    .padding(14.dp)
+            )
+        }
+      Text(text = "Susana Hoffs",
+          modifier = Modifier
+              .offset(x = 295.dp, y = 65.dp),
+            color = Color.White,
+          fontSize = 14.sp
+          )
+
+Row(
+modifier = Modifier
+    .fillMaxHeight()
+    .offset(y = 45.dp),
+    verticalAlignment = Alignment.CenterVertically
+
+
+)
+{
+    Icon(
+        imageVector = Icons.Default.LocationOn, contentDescription = "",
+        Modifier
+            .offset(x = 5.dp)
+            .width(20.dp),
+
+
+        )
+    Text(text = "You're in Paris",
+        color = Color.White)
+    Text(text = "My Tripps",
+        modifier = Modifier
+            .offset(x = -120.dp, y = 20.dp),
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color.White)
+
+         }
+      }
+
+        Text(text = "Categories")
+        LazyRow{
+
+            item(1){
+                Card(
+                    modifier = Modifier
+                        .height(100.dp)
+                        .width(150.dp)
+                        .padding(6.dp),
+
+                    colors = CardDefaults
+                        .cardColors(
+                            containerColor = Color(0xFFCE00F1),
+                            contentColor = Color(0xFFFFFFFFF)
+
+                            )
+
+
+                )
+
+                {
+                    Column(
+                        Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+
+                    ) {
+                        Icon(Icons.Default.Landscape, "",
+                            )
+                        Text(text = "Mountain")
+                    }
+
+                }
+            }
+
+            item(2){
+                Card(
+                    modifier = Modifier
+                        .height(100.dp)
+                        .width(150.dp)
+                        .padding(6.dp),
+                    colors = CardDefaults
+                        .cardColors(
+                            containerColor = Color(0xFFD992E6),
+                            contentColor = Color(0xFFFFFFFFF)
+                        )
+                ) {
+                    Column(
+                        Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Icon(Icons.Default.Snowboarding, "")
+                        Text(text = "Snow")
+                    }
+                }
+            }
+            item(3){
+                Card(
+                    modifier = Modifier
+                        .height(100.dp)
+                        .width(150.dp)
+                        .padding(6.dp),
+                    colors = CardDefaults
+                        .cardColors(
+                            containerColor = Color(0xFFD992E6),
+                            contentColor = Color(0xFFFFFFFFF)
+                        )
+                ) {
+                    Column(
+                        Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Icon(Icons.Default.BeachAccess, "")
+                        Text(text = "Beach")
+                    }
+                }
+            }
+
+        }
+        OutlinedTextField(value = "", onValueChange = {},
+            modifier = Modifier
+                .padding(start = 24.dp, top = 8.dp)
+                .width(350.dp),
+
+            label = {
+                Text(text = "Search your destiny")
+            },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedContainerColor = Color(0xFFFFFFFF),
+                unfocusedBorderColor = Color(0xF2EBDFDF)
+
+            ),
+            trailingIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "",
+                        tint = Color.Black
+                    )
+                }
+            },
+            shape = RoundedCornerShape(16.dp)
+            )
+        Text(text = "Past Trips",
+            modifier = Modifier
+                .padding(12.dp))
+        Card(
+            modifier = Modifier
+                .width(400.dp)
+                .height(300.dp)
+                .background(Color(0xF7DBDBDB))
+        ) {
+          Column {
+              Image(
+                  painter = painterResource(id = R.drawable.imagem1), contentDescription = "",
+                  modifier = Modifier
+                      .width(380.dp)
+                      .height(280.dp)
+                      .padding(10.dp, bottom = 125.dp)
+
+              )
+              Text(
+                  text = "London, 2019",
+                  fontSize = 16.sp,
+                  modifier = Modifier
+                      .offset(x = 10.dp, y = -130.dp),
+                  color = Color(0xFFCE00F1)
+              )
+          }
+Text(text = "London is the capital and largest city of  the United Kingdom, with a population of just under 9 million.",
+    modifier = Modifier.offset(x = 0.dp, y = 100.dp))
+
+        }
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(150.dp)
+                .padding(top = 10.dp)
+        ) {
+
+        }
+
+
+
+
+
+
+
+
+
+
+    
+    }
+
+  }
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun TelaHomePreview(){
+    TripprommTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            TelaHome()
+        }
     }
 }
